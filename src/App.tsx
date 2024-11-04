0c39cbd354c41c17cdd2e41b8ad159905fbb52ba
Nathan Britten <nathan.britten@adarga.ai>
2024-11-04 21:06:18 +0000

    feat: Initial Project setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Consultation } from "./pages/consultation";
+
+function App() {
+  return (
+    <div>
+      <Consultation />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/consultation.tsx
@@ -0,0 +1,8 @@
+export function Consultation() {
+  return (
+    <div>
+      <h1>Consultation</h1>
+      <p>This is a consultation page.</p>
+    </div>
+  );
+}
